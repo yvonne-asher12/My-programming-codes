99aa3cd4d1586f43fbffe0e83b38d5af9939148c
yvonne-asher12 <yvonneasher46@gmail.com>
2025-03-02 12:36:39 +0300

    fourth

---
+++ inhe.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+	protected:
+		string name;
+	public:
+		void setName(string n){
+			name= n;
+		}	
+	string getName(){
+		return name;
+	}
+};
+
+class LibraryMember :public Person{
+	private:
+		int memberID;
+		int booksBorrowed;
+	public:
+		LibraryMember(string n,int id,int borrowed){
+			name =n;
+			memberID=id;
+			booksBorrowed=borrowed;
+		}	
+	public:
+		int getMemberID(){
+			return memberID;
+		}	
+
+        int getBooksBorrowed(){
+			return booksBorrowed;
+		}
+};
+
+class PremiumMember : public LibraryMember {
+private:
+    double membershipFee;
+
+public:
+    PremiumMember(string n, int id, int borrowed, double fee)
+        : LibraryMember(n, id, borrowed), membershipFee(fee) {}
+
+    double getMembershipFee() {
+        return membershipFee;
+    }
+};
+
+int main() {
+    PremiumMember member("yvonne", 450100, 7, 170.85);
+
+    
+    cout << "Name: " << member.getName() << endl;
+    cout << "Member ID: " << member.getMemberID() << endl;
+    cout << "Books Borrowed: " << member.getBooksBorrowed() << endl;
+    cout << "Membership Fee: ksh" << member.getMembershipFee() << endl;
+
+    return 0;
+}		 
