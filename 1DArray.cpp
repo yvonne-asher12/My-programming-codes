73c0451a5f99fbd5ac4ca4c651e5346ac677c85a
yvonne-asher12 <yvonneasher46@gmail.com>
2025-03-04 22:42:37 +0300

    fifth

---
+++ 1DArray.cpp
@@ -0,0 +1,18 @@
+/*
+C ++ Program to show 2D Array
+NAME: Yvonne Karimi
+Reg No : BSCIT-05-0072/2024
+Date : 03/03/2025
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	int j;
+    int marks[10] = {2,4,5,7,8,4};
+    for(j=0;j<10;j++){
+        cout<<marks[j]<<"\t";
+        }
+        return 0;
+        
+    }
